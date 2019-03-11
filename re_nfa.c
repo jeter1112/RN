440372e7f577732083e73b764d0dd935a1e5b266
jeter1112 <jeter023217@gmail.com>
2019-03-11 20:27:37 +0800

    code start

--- re_nfa.c
+++ re_nfa.c
@@ -1,1 +1,27 @@
 #include"re_nfa.h"
+
+/*********
+ * static method declaireration
+ * 
+ * */
+
+
+
+
+
+
+
+/******* static method implementation  **/
+
+
+
+
+
+
+/*** API implementation ****/
+
+
+char* infix_to_postfix(char* infix)
+{
+    return 0;
+}
--- re_nfa.h
+++ re_nfa.h
@@ -5,13 +5,27 @@
 struct State;
 typedef struct State state;
 
+struct State
+{
+    int charnum;  //char 1 byte; int 4 byte;  byte duiqi;
+
+    state* out;
+    state* out1;
+};
+
+
+
+
+
+
+
 
 
 char* infix_to_postfix(char*);
 
 state* RE_to_NFA(char*);
 
-bool RE_macth_string(char*,State*);
+int RE_macth_string(char*,state*);
 
 
 
